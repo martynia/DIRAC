c26728aaf130064962ca4c2128d868f20f325962
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-06-03 14:57:07 +0100

    fix multi VO metadata to be really a multi V, not multi group

--- DataManagementSystem/DB/FileCatalogComponents/MultiVOFileMetadata.py
+++ DataManagementSystem/DB/FileCatalogComponents/MultiVOFileMetadata.py
@@ -1,35 +1,34 @@
 """ DIRAC Multi VO FileCatalog plugin class to manage file metadata for multiple VO.
 """
 
-
 __RCSID__ = "$Id$"
 
 from FileMetadata import FileMetadata
 
+
 class MultiVOFileMetadata(FileMetadata):
+  """
+  MULti-VO FileCatalog plugin implementation.
+  """
+
+  def __init__(self, database=None):
+    FileMetadata.__init__(self, database=database)
+
+  def getMetaName(self, meta, credDict):
     """
-    MULti-VO FileCatalog plugin implementation.
+    Return a fully-qualified metadata name based on client-suplied metadata name and
+    client credentials. User group is added to the metadata passed in.
+    :param meta: metadata name
+    :param credDict: client credentials
+    :return: fully-qualified metadata name
     """
 
-    def __init__( self, database = None ):
+    return meta + self.getMetaNameSuffix(credDict)
 
-        FileMetadata.__init__(self, database=database)
-
-    def getMetaName(self, meta, credDict):
-        """
-        Return a fully-qualified metadata name based on client-suplied metadata name and
-        client credentials. User group is added to the metadata passed in.
-        :param meta: metadata name
-        :param credDict: client credentials
-        :return: fully-qualified metadata name
-        """
-
-        return meta+self.getMetaNameSuffix(credDict)
-
-    def getMetaNameSuffix(self, credDict):
-        """
-        Get a VO specific suffix from user credentials.
-        :param credDict: user credentials
-        :return: VO specific suffix
-        """
-        return '_'+credDict['group']
+  def getMetaNameSuffix(self, credDict):
+    """
+    Get a VO specific suffix from user credentials.
+    :param credDict: user credentials
+    :return: VO specific suffix
+    """
+    return '_' + credDict['group']
