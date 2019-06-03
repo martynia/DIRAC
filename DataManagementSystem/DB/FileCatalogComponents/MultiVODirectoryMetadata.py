cb93316451abae8f9146686405a97f76a6f46156
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-06-03 14:57:07 +0100

    fix multi VO metadata to be really a multi V, not multi group

--- DataManagementSystem/DB/FileCatalogComponents/MultiVODirectoryMetadata.py
+++ DataManagementSystem/DB/FileCatalogComponents/MultiVODirectoryMetadata.py
@@ -4,31 +4,36 @@
 __RCSID__ = "$Id$"
 
 from DirectoryMetadata import DirectoryMetadata
+from DIRAC.ConfigurationSystem.Client.Helpers import Registry
 
 
 class MultiVODirectoryMetadata(DirectoryMetadata):
+  """
+  MULti-VO FileCatalog plugin implementation.
+  """
+
+  def __init__(self, database=None):
+    DirectoryMetadata.__init__(self, database=database)
+
+  def getMetaName(self, meta, credDict):
     """
-    MULti-VO FileCatalog plugin implementation.
+    Return a fully-qualified metadata name based on client-suplied metadata name and
+    client credentials. User VO is added to the metadata passed in.
+
+    :param meta: metadata name
+    :param credDict: client credentials
+    :return: fully-qualified metadata name
     """
 
-    def __init__(self, database=None):
-        DirectoryMetadata.__init__(self, database=database)
+    return meta + self.getMetaNameSuffix(credDict)
 
-    def getMetaName(self, meta, credDict):
-        """
-        Return a fully-qualified metadata name based on client-suplied metadata name and
-        client credentials. User group is added to the metadata passed in.
-        :param meta: metadata name
-        :param credDict: client credentials
-        :return: fully-qualified metadata name
-        """
+  def getMetaNameSuffix(self, credDict):
+    """
+    Get a VO specific suffix from user credentials and the CS.
 
-        return meta + self.getMetaNameSuffix(credDict)
+    :param credDict: user credentials
+    :return: VO specific suffix
+    """
 
-    def getMetaNameSuffix(self, credDict):
-        """
-        Get a VO specific suffix from user credentials.
-        :param credDict: user credentials
-        :return: VO specific suffix
-        """
-        return '_' + credDict['group']
+    vo = Registry.getGroupOption(credDict['group'], 'VO')
+    return '_' + vo
