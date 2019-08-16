ebc3509b9f9968aad62a72baf2046f3879b3d7a5
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-08-16 16:01:39 +0100

    Multi VO  inheritance diamond with super fix 1

--- DataManagementSystem/DB/FileCatalogComponents/MultiVOMetaNameMixIn.py
+++ DataManagementSystem/DB/FileCatalogComponents/MultiVOMetaNameMixIn.py
@@ -12,8 +12,8 @@ class MultiVOMetaNameMixIn(MetaNameMixIn):
   MULti-VO MetaName MixIn implementation.
   """
 
-  def __init__(self):
-    super(MultiVOMetaNameMixIn, self).__init__()
+  def __init__(self, *args, **kwargs):
+    super(MultiVOMetaNameMixIn, self).__init__(*args, **kwargs)
 
   def getMetaName(self, meta, credDict):
     """
