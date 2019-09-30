bca06df6fb673b738555d078e77b70cab30f53ce
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-09-30 13:37:08 +0100

    drop inheritance from FileCatalogClientBase, object instead

--- Resources/Catalog/RucioFileCatalogClient.py
+++ Resources/Catalog/RucioFileCatalogClient.py
@@ -7,7 +7,7 @@ from __future__ import division
 import os
 
 from DIRAC import S_OK, S_ERROR, gLogger
-from DIRAC.Resources.Catalog.Utilities import checkCatalogArguments
+#from DIRAC.Resources.Catalog.Utilities import checkCatalogArguments
 from DIRAC.ConfigurationSystem.Client.Helpers.Registry import getDNForUsername, getVOMSAttributeForGroup, \
   getVOForGroup, getVOOption
 #from DIRAC.Resources.Catalog.FileCatalogClientBase import FileCatalogClientBase
@@ -18,28 +18,28 @@ class RucioFileCatalogClient(Object):
 
   """
 
-  READ_METHODS = FileCatalogClientBase.READ_METHODS + ['listDirectory', 'getUserDirectory']
+  READ_METHODS = ['listDirectory', 'getUserDirectory']
 
 
-  WRITE_METHODS = FileCatalogClientBase.WRITE_METHODS + ['addFile']
+  WRITE_METHODS = ['addFile']
 
-  NO_LFN_METHODS = FileCatalogClientBase.NO_LFN_METHODS + ['getUserDirectory', 'createUserDirectory',
+  NO_LFN_METHODS =  ['getUserDirectory', 'createUserDirectory',
                                                          'createUserMapping', 'removeUserDirectory']
 
-  ADMIN_METHODS = FileCatalogClientBase.ADMIN_METHODS + ['getUserDirectory']
+  ADMIN_METHODS =  ['getUserDirectory']
 
   def __init__( self, url=None,  **options ):
     self.serverURL = 'DataManagement/RucioFileCatalog' if not url else url
     super(RucioFileCatalogClient, self).__init__(self.serverURL, **options)
     gLogger.debug("Rucio File Catalog client created with options: ", options)
 
-  @checkCatalogArguments
+  # @checkCatalogArguments
   def listDirectory( self, lfns, verbose = False ):
     gLogger.debug("Rucio list directory for lfns: ", lfns)
     if verbose:
       pass
 
-  @checkCatalogArguments
+  # @checkCatalogArguments
   def isFile(self, lfns, timeout=120):
     """ Check whether the supplied lfns are files """
     gLogger.debug("Rucio is file (lfns): ", lfns)
