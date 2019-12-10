cc3d4cbab9280b8f307a8947aee2684e51733927
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-12-10 17:32:31 +0000

    add getGrouppedPilotSummary to the PilotManagerHandler

--- WorkloadManagementSystem/Service/PilotManagerHandler.py
+++ WorkloadManagementSystem/Service/PilotManagerHandler.py
@@ -195,6 +195,14 @@ class PilotManagerHandler(RequestHandler):
     return result
 
   ##############################################################################
+  types_getGrouppedPilotSummary = [dict, list]
+
+  @classmethod
+  def getGrouppedPilotSummary(cls, selectDict, columnList):
+    result = pilotDB.getGrouppedPilotSummary(selectDict, columnList)
+    return result
+
+  ##############################################################################
   types_getPilots = [(basestring, int, long)]
 
   @classmethod
