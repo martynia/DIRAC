b1711394b57fbc2e3139d98a516b83047f0084d3
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-12-10 17:06:00 +0000

    test pivoted pilot summary table

---
+++ WorkloadManagementSystem/DB/PivotedPilotSummaryTable.py
@@ -0,0 +1,150 @@
+from DIRAC import S_OK, S_ERROR
+from DIRAC.Core.Base.DB import DB
+import DIRAC.Core.Utilities.Time as Time
+from DIRAC.Core.Utilities.MySQL import _quotedList
+
+class PivotedPilotSummaryTable(DB):
+  """
+    SELECT Pivoted_eff.GridSite, Pivoted_eff.DestinationSite, Pivoted_eff.Done_Empties,
+           Pivoted_eff.Submitted, Pivoted_eff.Done, Pivoted_eff.Failed, Pivoted_eff.Aborted, 
+           Pivoted_eff.Running, Pivoted_eff.Waiting, Pivoted_eff.Scheduled, Pivoted_eff.Total,
+    (CASE 
+         WHEN Pivoted_eff.Done - Pivoted_eff.Done_Empties > 0 
+              THEN Pivoted_eff.Done/(Pivoted_eff.Done-Pivoted_eff.Done_Empties)
+         WHEN Pivoted_eff.Done=0 
+              THEN 0 
+         WHEN Pivoted_eff.Done=Pivoted_eff.Done_Empties 
+              THEN 99.0 ELSE 0.0 END) 
+          AS PilotsPerJob,
+          (Pivoted_eff.Total - Pivoted_eff.Aborted)/Pivoted_eff.Total*100.0 AS Eff
+    FROM (SELECT Frank.GridSite, Frank.DestinationSite, 
+                 SUM(if (Frank.Status='Done', Frank.Empties,0)) AS Done_Empties,
+                 SUM(if (Frank.Status='Submitted', Frank.qty, 0)) AS Submitted,
+                 SUM(if (Frank.Status='Done', Frank.qty, 0)) AS Done,
+                 SUM(if (Frank.Status='Failed', Frank.qty, 0)) AS Failed,
+                 SUM(if (Frank.Status='Aborted', Frank.qty, 0)) AS Aborted,
+                 SUM(if (Frank.Status='Running', Frank.qty, 0)) AS Running,
+                 SUM(if (Frank.Status='Waiting', Frank.qty, 0)) AS Waiting,
+                 SUM(if (Frank.Status='Scheduled', Frank.qty, 0)) AS Scheduled,
+                 SUM(Frank.qty) AS Total
+          FROM (SELECT GridSite, DestinationSite, Status, count(CASE WHEN CurrentJobID=0  THEN 1 END) AS Empties,
+                count(*) AS qty
+                from PilotAgents where 
+                     Status not in ('Done', 'Aborted') OR (Status in ('Done', 'Aborted')
+                     AND LastUpdateTime > '2019-12-04 10:35:00')  group by GridSite, DestinationSite, Status)
+          AS Frank
+          group by GridSite, DestinationSite)  
+    AS Pivoted_eff;
+  """
+
+  def __init__(self, columnList=None):
+    """
+    Initialise a table with columns to be groupped by.
+
+    :param columnList:
+    :return:
+    """
+
+    self.columnList = columnList
+    if self.columnList is None:
+      self.columnList = ['GridSite', 'DestinationSite']
+
+    self.pstates = ['Submitted', 'Done', 'Failed', 'Aborted',
+                    'Running', 'Waiting', 'Scheduled', 'Ready']
+
+    self.columns = self.columnList + self.pstates # MySQL._query does not give us column names, sadly.
+
+  def buildSQL(self, selectDict = None):
+    """
+    Build an SQL query to create a table with all status counts in one row, ("pivoted")
+    grouped by columns in the column list.
+    The default list is ['GridSite', 'DestinationSite']
+
+    :param selectDict:
+    :return: SQL query
+    """
+
+    last_update = Time.dateTime() - Time.day
+    # TODO
+    # cond = buildCondition(...)
+    pvtable = 'pivoted'
+    innerGroupBy = "(SELECT %s, Status,\n " \
+                   "count(CASE WHEN CurrentJobID=0  THEN 1 END) AS Empties," \
+                   " count(*) AS qty FROM PilotAgents\n " \
+                   "WHERE Status NOT IN ('Done', 'Aborted') OR (Status in ('Done', 'Aborted') \n" \
+                   " AND \n" \
+                   " LastUpdateTime > '%s')" \
+                   " GROUP by %s, Status)\n AS %s" % (
+                     _quotedList(self.columnList), last_update,
+                     _quotedList(self.columnList), pvtable)
+
+    # pivoted table: combine records with the same group of self.columnList into a single row.
+
+
+    # print _quotedList(pstates)
+    # pivotedQuery = "SELECT %s,\n" % _quotedList([pvtable + '.' + item for item in self.columnList])
+    # FIXME backqouting does not work with a prefix. Investigate.
+    pivotedQuery = "SELECT %s,\n" % ', '.join([pvtable + '.' + item for item in self.columnList])
+    line_template = "  SUM(if (pivoted.Status={state!r}, pivoted.qty, 0)) AS {state}"
+    pivotedQuery += ',\n'.join(line_template.format(state=state) for state in self.pstates)
+    pivotedQuery += ",\n  SUM(if (%s.Status='Done', %s.Empties,0)) AS Done_Empty,\n" \
+                    "  SUM(%s.qty) AS Total " \
+                    "FROM\n" % (pvtable, pvtable, pvtable)
+
+    outerGroupBy = " GROUP BY %s) \nAS pivoted_eff;" % _quotedList(self.columnList)
+
+    # add efficiency columns using aliases defined in the pivoted table
+    eff_case = "(CASE\n  WHEN pivoted_eff.Done - pivoted_eff.Done_Empty > 0 \n" \
+               "  THEN pivoted_eff.Done/(pivoted_eff.Done-pivoted_eff.Done_Empty) \n" \
+               "  WHEN pivoted_eff.Done=0 THEN 0 \n" \
+               "  WHEN pivoted_eff.Done=pivoted_eff.Done_Empty \n" \
+               "  THEN 99.0 ELSE 0.0 END) AS PilotsPerJob,\n" \
+               " (pivoted_eff.Total - pivoted_eff.Aborted)/pivoted_eff.Total*100.0 AS Eff \nFROM \n("
+    eff_select_template = " pivoted_eff.{state}"
+    # now select the columns + the states:
+    pivoted_eff = "SELECT %s,\n" % ', '.join(['pivoted_eff' + '.' + item for item in self.columnList]) + \
+                  ', '.join(eff_select_template.format(state=state) for state in self.pstates + ['Total']) + ", \n"
+
+    finalQuery = pivoted_eff + eff_case + pivotedQuery + innerGroupBy + outerGroupBy
+    self.columns += self.columns+[' Total', 'PilotsPerJob', 'Eff']
+    return finalQuery
+
+  def query(self, cmd):
+    res = self._query(cmd)
+    if not res['OK']:
+      return res
+# TODO add site or CE status, while looping
+    rows = []
+    result = {'ParameterNames': self.columnList}
+    for row in res['Value']:
+      rows.append(row)
+
+    result['Records': rows]
+    return S_OK(result)
+
+"""
+def _quotedList(fieldList=None):
+  """
+    Quote a list of MySQL Field Names with "`"
+    Return a comma separated list of quoted Field Names
+
+    To be use for Table and Field Names
+  """
+  if fieldList is None:
+    return None
+  quotedFields = []
+  try:
+    for field in fieldList:
+      quotedFields.append('`%s`' % field.replace('`', ''))
+  except Exception:
+    return None
+  if not quotedFields:
+    return None
+
+  return ', '.join(quotedFields)
+
+
+obj = PivotedPilotSummaryTable()
+print (obj.buildSQL(None))
+
+"""
