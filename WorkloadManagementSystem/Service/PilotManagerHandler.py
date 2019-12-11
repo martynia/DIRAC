32e4120b77b1968965c837fcbb2fdcee6878df59
Janusz Martyniak <janusz.martyniak@googlemail.com>
2019-12-11 11:10:55 +0000

    a typo ..

--- WorkloadManagementSystem/Service/PilotManagerHandler.py
+++ WorkloadManagementSystem/Service/PilotManagerHandler.py
@@ -199,7 +199,7 @@ class PilotManagerHandler(RequestHandler):
 
   @classmethod
   def export_getGroupedPilotSummary(cls, selectDict, columnList):
-    result = pilotDB.getGrouppedPilotSummary(selectDict, columnList)
+    result = pilotDB.getGroupedPilotSummary(selectDict, columnList)
     return result
 
   ##############################################################################
