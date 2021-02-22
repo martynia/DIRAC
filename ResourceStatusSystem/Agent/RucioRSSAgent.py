85478950347ee216b99a00643285a2ceccfed305
serfon <cedric.serfon@desy.de>
2021-02-22 15:45:17 +0100

    Add RucioSynchronizer and RucioRSSAgent agents

---
+++ ResourceStatusSystem/Agent/RucioRSSAgent.py
@@ -0,0 +1,69 @@
+""" :mod: RucioRSSAgent
+
+    Agent that synchronizes Rucio and Dirac
+"""
+
+# # imports
+from DIRAC import S_OK, S_ERROR
+from DIRAC import gConfig, gLogger
+from DIRAC.Core.Base.AgentModule import AgentModule
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.ResourceStatusSystem.Client.ResourceStatus import ResourceStatus
+
+from rucio.client import Client
+
+__RCSID__ = "Id$"
+
+class RucioRSSAgent(AgentModule):
+  """
+  .. class:: RucioRSSAgent
+
+  Agent that synchronizes Rucio and Dirac
+  """
+
+  def initialize(self):
+    """ agent's initalisation
+
+    :param self: self reference
+    """
+    gLogger.info("Starting RucioRSSAgent")
+    return S_OK()
+
+  def execute(self):
+    """ execution in one agent's cycle
+
+    :param self: self reference
+    """
+    rSS = ResourceStatus()
+    client = Client()
+    try: 
+      for rse in client.list_rses():
+        thisSe = rse['rse']
+        gLogger.info("Running on %s" % thisSe)
+        resStatus =  rSS.getElementStatus(thisSe, "StorageElement")
+        dictSe = client.get_rse(thisSe)
+        if resStatus['OK']:
+          seAccessValue = resStatus['Value'][thisSe]
+          availabilityRead = True if seAccessValue['ReadAccess'] == 'Active' else False
+          availabilityWrite = True if seAccessValue['WriteAccess'] == 'Active' else False
+          availabilityDelete = True if seAccessValue['RemoveAccess'] == 'Active' else False
+          isUpdated = False
+          if dictSe['availability_read'] != availabilityRead:
+            gLogger.info('Set availability_read for %s to %s' % (thisSe, availabilityRead))
+            client.update_rse(thisSe, {'availability_read': availabilityRead})
+            isUpdated = True
+          if dictSe['availability_write'] != availabilityWrite:
+            gLogger.info('Set availability_write for %s to %s' % (thisSe, availabilityWrite))
+            client.update_rse(thisSe, {'availability_write': availabilityWrite})
+            isUpdated = True
+          if dictSe['availability_delete'] != availabilityDelete:
+            gLogger.info('Set availability_delete for %s to %s' % (thisSe, availabilityDelete))
+            client.update_rse(thisSe, {'availability_delete': availabilityDelete})
+            isUpdated = True
+          #### TEMPORARY FIX
+          if isUpdated:
+            client.add_rse_attribute(rse, rse, True)
+          #### TEMPORARY FIX
+    except Exception as err:
+      return S_ERROR(str(err))
+    return S_OK()
