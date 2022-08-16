1c56081576930ae729f0a907ed5f05fc3540f123
martynia <janusz.martyniak@googlemail.com>
2022-08-16 16:26:50 +0100

    test: A simple Tornado pilot logging client test (black)

--- src/DIRAC/WorkloadManagementSystem/Client/test/Test_TornadoPilotLoggingClient.py
+++ src/DIRAC/WorkloadManagementSystem/Client/test/Test_TornadoPilotLoggingClient.py
@@ -1,15 +1,18 @@
 import unittest
 from unittest.mock import patch
+from unittest.mock import MagicMock
 
-from DIRAC.WorkloadManagementSystem.Client.TornadoPilotLoggingClient import TornadoPilotLoggingClient
+import DIRAC.WorkloadManagementSystem.Client.TornadoPilotLoggingClient as tplc
 
 
 class TestTornadoPilotLoggingClient(unittest.TestCase):
-    @patch("TornadoClient")
+    @patch.object(tplc.TornadoPilotLoggingClient, "executeRPC", return_value={"key": "value"})
     def test_client(self, clientMock):
-        client = TornadoPilotLoggingClient("test.server", useCertificates=True)
-        client.getMetadata()
-        clientMock.executeRPC.assert_called()
+        print("from test", clientMock)
+        client = tplc.TornadoPilotLoggingClient("test.server", useCertificates=True)
+        res = client.getMetadata()
+        clientMock.assert_called_with("getMetadata")
+        self.assertEqual(res, clientMock.return_value)
 
 
 if __name__ == "__main__":
