06e4be3f81c763bca71d9b549251206b7cefa422
martynia <janusz.martyniak@googlemail.com>
2022-08-17 12:27:37 +0100

    test: A simple Tornado pilot logging client test

--- src/DIRAC/WorkloadManagementSystem/Client/test/Test_TornadoPilotLoggingClient.py
+++ src/DIRAC/WorkloadManagementSystem/Client/test/Test_TornadoPilotLoggingClient.py
@@ -8,7 +8,6 @@ import DIRAC.WorkloadManagementSystem.Client.TornadoPilotLoggingClient as tplc
 class TestTornadoPilotLoggingClient(unittest.TestCase):
     @patch.object(tplc.TornadoPilotLoggingClient, "executeRPC", return_value={"key": "value"})
     def test_client(self, clientMock):
-        print("from test", clientMock)
         client = tplc.TornadoPilotLoggingClient("test.server", useCertificates=True)
         res = client.getMetadata()
         clientMock.assert_called_with("getMetadata")
